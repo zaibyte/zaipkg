0cd439414c7e9028a4837bd19d60a0a7556b16d8
xiangxi <xiangxi.xu@tesamc.com>
2020-11-19 10:36:37 +0800

    xruntime: Add AutoGomaxPROCS function

---
+++ xruntime/xruntime.go
@@ -0,0 +1,21 @@
+// Package xruntime provides helper tools for Go Runtime.
+package xruntime
+
+import "runtime"
+
+// AutoGOMAXPROCS sets the maximum number of CPUs that can be executing
+// simultaneously automatically. If n < 1, it does not
+// change the current setting.
+// The number of logical CPUs on the local machine can be queried with NumCPU.
+// This call will go away when the scheduler improves.
+func AutoGOMAXPROCS() {
+
+	p := runtime.NumCPU()
+	// Actually Go can't handle multi-cores well enough,
+	// in ByteDance(A large internet Company which uses Go heavily) Graph Database team,
+	// one Go process will only have 20 cores at most.
+	if p > 32 {
+		p = 32
+	}
+	runtime.GOMAXPROCS(p)
+}
