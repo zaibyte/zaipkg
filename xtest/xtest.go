c4d338c4a04c4bcf1ea7e6e8d202b53d59868ec8
xiangxi <xiangxi.xu@tesamc.com>
2020-11-25 17:44:24 +0800

    xtest: usage of properties flag

--- xtest/xtest.go
+++ xtest/xtest.go
@@ -7,6 +7,11 @@ import (
 var _propEnabled = flag.Bool("xtest.prop", false, "enable properties testing or not")
 
 // IsPropEnabled returns enable properties testing or not.
+// Default is false.
+//
+// e.g.
+// no properties testing: go test -prop=false -v or go test -v
+// run properties testing: go test -prop=true -v
 func IsPropEnabled() bool {
 	if !flag.Parsed() {
 		flag.Parse()
