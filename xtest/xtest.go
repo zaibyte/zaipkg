f9d5d3a57fb5f01aa828a13fd5636842a7038848
xiangxi <xiangxi.xu@tesamc.com>
2020-11-25 17:46:59 +0800

    xtest: typo

--- xtest/xtest.go
+++ xtest/xtest.go
@@ -10,8 +10,8 @@ var _propEnabled = flag.Bool("xtest.prop", false, "enable properties testing or 
 // Default is false.
 //
 // e.g.
-// no properties testing: go test -prop=false -v or go test -v
-// run properties testing: go test -prop=true -v
+// no properties testing: go test -xtest.prop=false -v or go test -v
+// run properties testing: go test -xtest.prop=true -v
 func IsPropEnabled() bool {
 	if !flag.Parsed() {
 		flag.Parse()
