912d4f84ba22d9582afa0882f455335fba177286
xiangxi <xiangxi.xu@tesamc.com>
2021-01-12 12:59:12 +0800

    xnet/xhttp: add uint32 in ParsePath

--- xnet/xhttp/xhttp.go
+++ xnet/xhttp/xhttp.go
@@ -79,6 +79,8 @@ func ParsePath(p httprouter.Params, name string, val interface{}) {
 		}
 	case *uint16:
 		*v = cast.ToUint16(vs)
+	case *uint32:
+		*v = cast.ToUint32(vs)
 	case *uint64:
 		vuint, err := strconv.ParseUint(vs, 10, 64)
 		if err == nil {
