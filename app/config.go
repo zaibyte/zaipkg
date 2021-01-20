ec1b43842a75f148c2a0bfecf0d7ab65c2cfdb8b
xiangxi <xiangxi.xu@tesamc.com>
2021-01-20 12:51:28 +0800

    app: using uint32 as InstanceID

--- app/config.go
+++ app/config.go
@@ -10,7 +10,7 @@ type Config struct {
 	// boxID: [1, 255)
 	BoxID int64 `toml:"box_id"`
 	// Every instance has its own unique instanceID
-	InstanceID string `toml:"instance-id"`
+	InstanceID uint32 `toml:"instance-id"`
 	// Every instance belongs to a certain rack.
 	// RackID: [1, ?)
 	// 0 is reserved, it's default rack, means ignore rack location.
