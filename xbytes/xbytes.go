0a0fd3380a524bb02e385ca03e09e513f20a4337
xiangxi <xiangxi.xu@g.tesamc.com>
2021-02-20 18:37:33 +0800

    xbytes: AlignSize add case for n <= 0

--- xbytes/xbytes.go
+++ xbytes/xbytes.go
@@ -3,5 +3,8 @@ package xbytes
 // AlignSize calculates size after align n to align.
 // The return will be the multiple of align.
 func AlignSize(n int64, align int64) int64 {
+	if n <= 0 {
+		return align
+	}
 	return (n + align - 1) &^ (align - 1)
 }
