46e956bddcc03a2c26ff632ff8751d405803b775
xiangxi <xiangxi.xu@g.tesamc.com>
2021-04-19 07:10:58 +0800

    metricutil: add MakeLabel

--- metricutil/util.go
+++ metricutil/util.go
@@ -9,11 +9,15 @@ import (
 // MakeMetricName makes github.com/VictoriaMetrics/metrics style metric name.
 // e.g. `queue_size{queue="foobar",topic="baz"}`
 func MakeMetricName(name string, labels map[string]string) string {
+	l := MakeLabel(labels)
+	return fmt.Sprintf(`%s{%s}`, name, l)
+}
+
+func MakeLabel(labels map[string]string) string {
 	ls := make([]string, 0, len(labels))
 	for k, v := range labels {
 		ls = append(ls, fmt.Sprintf(`%s="%s"`, k, v))
 	}
 	sort.Strings(ls) // MakeMetricName will only run one time, the cost of sort is okay, but easier to read & test.
-	l := strings.Join(ls, ",")
-	return fmt.Sprintf(`%s{%s}`, name, l)
+	return strings.Join(ls, ",")
 }
