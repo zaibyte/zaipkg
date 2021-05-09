e1424b2b28969d359f90a5536c557a0305c35eee
xiangxi <xiangxi.xu@g.tesamc.com>
2021-05-10 05:13:24 +0800

    xbytes: add MakeAlignedBlock

--- xbytes/xbytes_test.go
+++ xbytes/xbytes_test.go
@@ -1,6 +1,9 @@
 package xbytes
 
-import "testing"
+import (
+	"testing"
+	"unsafe"
+)
 
 func TestAlignSize(t *testing.T) {
 	var align int64 = 1 << 12
@@ -15,6 +18,15 @@ func TestAlignSize(t *testing.T) {
 		n := AlignSize(i, align)
 		if n != align*2 {
 			t.Fatal("align mismatch")
+		}
+	}
+}
+
+func TestAlignedBlock(t *testing.T) {
+	for i := 1; i < 33; i++ {
+		b := MakeAlignedBlock(i, 16)
+		if uintptr(unsafe.Pointer(&b[0]))&15 != 0 {
+			t.Fatal("aligned mismatch")
 		}
 	}
 }
