daa06dde9923369c45155131bb9960fd259886c8
xiangxi <xiangxi.xu@tesamc.com>
2021-06-11 10:20:24 +0800

    zbufutil: add state condition func for SyncMeta

--- zbufutil/meta.go
+++ zbufutil/meta.go
@@ -35,3 +35,19 @@ func (p *SyncMeta) SetState(state metapb.ZBufState) (ok bool, oldState metapb.ZB
 
 	return atomic.CompareAndSwapInt32((*int32)(&p.State), int32(oldState), int32(state)), oldState
 }
+
+func (p *SyncMeta) IsTombstone() bool {
+	return p.GetState() == metapb.ZBufState_ZBuf_Tombstone
+}
+
+func (p *SyncMeta) IsDown() bool {
+	return p.GetState() == metapb.ZBufState_ZBuf_Down
+}
+
+func (p *SyncMeta) IsDisconnected() bool {
+	return p.GetState() == metapb.ZBufState_ZBuf_Disconnected
+}
+
+func (p *SyncMeta) IsOffline() bool {
+	return p.GetState() == metapb.ZBufState_ZBuf_Offline
+}
