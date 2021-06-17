e538db85842e92742862915f03eae0debb8f294f
xiangxi <xiangxi.xu@tesamc.com>
2021-06-17 16:23:12 +0800

    uid: add GenSeqInstanceID

--- uid/instanceid.go
+++ uid/instanceid.go
@@ -37,7 +37,40 @@ func GetMachineFromInstanceID(instanceID string) string {
 
 // GenRandInstanceID generates an instance_id for testing only.
 func GenRandInstanceID() string {
-	return fmt.Sprintf("cn-sz-%03d-%04d", xrand.Int63n(1000), xrand.Int63n(1000))
+
+	return MakeInstanceID("cn", "sz", xrand.Int63n(1000), xrand.Int63n(1000))
+}
+
+// GenSeqInstanceID generates sequential machine number(from 1) with default idc_code instance_ids.
+func GenSeqInstanceID(cnt int) []string {
+
+	ret := make([]string, cnt)
+
+	for i := 1; i <= cnt; i++ {
+		ret[i-1] = MakeInstanceID("cn", "sz", 1, int64(i))
+	}
+	return ret
+}
+
+func MakeInstanceID(region, city string, idc, machine int64) string {
+
+	if len(region) != 2 {
+		panic("illegal region")
+	}
+
+	if len(city) != 2 {
+		panic("illegal city")
+	}
+
+	if idc > 999 {
+		panic("illegal idc")
+	}
+
+	if machine > 9999 {
+		panic("illegal machine")
+	}
+
+	return fmt.Sprintf("%s-%s-%03d-%04d", region, city, idc, machine)
 }
 
 var InstanceIDRegexp = regexp.MustCompile(`^[a-z]{2}-[a-z]{2}-\d{3}-\d{4}`)
