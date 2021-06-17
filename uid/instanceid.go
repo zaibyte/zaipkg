51ae90f74633af5e981dc8e5336880a873dce9f7
xiangxi <xiangxi.xu@tesamc.com>
2021-06-17 14:40:33 +0800

    uid: using 4chars as machine number in instance_id

--- uid/instanceid.go
+++ uid/instanceid.go
@@ -8,11 +8,11 @@ import (
 	"g.tesamc.com/IT/zaipkg/xmath/xrand"
 )
 
-const InstanceIDLen = 16
+const InstanceIDLen = 14
 
 // GetIDCFromInstanceID gets idc label from instance_id.
 // <region>-<city>-<idc_number>-<machine_number>
-// e.g. cn-sz-001-000001, the return value will be cn-sz-001
+// e.g. cn-sz-001-0001, the return value will be cn-sz-001
 func GetIDCFromInstanceID(instanceID string) string {
 
 	ss := strings.Split(instanceID, "-")
@@ -25,7 +25,7 @@ func GetIDCFromInstanceID(instanceID string) string {
 
 // GetMachineFromInstanceID gets machine_number from instance_id.
 // <region>-<city>-<idc_number>-<machine_number>
-// e.g. cn-sz-001-0000001, the return value will be 0000001
+// e.g. cn-sz-001-0001, the return value will be 0001
 func GetMachineFromInstanceID(instanceID string) string {
 	ss := strings.Split(instanceID, "-")
 	if len(ss) != 4 {
@@ -37,10 +37,10 @@ func GetMachineFromInstanceID(instanceID string) string {
 
 // GenRandInstanceID generates an instance_id for testing only.
 func GenRandInstanceID() string {
-	return fmt.Sprintf("cn-sz-%03d-%06d", xrand.Int63n(1000), xrand.Int63n(1000000))
+	return fmt.Sprintf("cn-sz-%03d-%04d", xrand.Int63n(1000), xrand.Int63n(1000))
 }
 
-var InstanceIDRegexp = regexp.MustCompile(`^[a-z]{2}-[a-z]{2}-\d{3}-\d{6}`)
+var InstanceIDRegexp = regexp.MustCompile(`^[a-z]{2}-[a-z]{2}-\d{3}-\d{4}`)
 
 // IsValidInstanceID returns the instanceID is valid in Zai or not.
 func IsValidInstanceID(instanceID string) bool {
