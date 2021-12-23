329c9302699a3e2c5a2063656a562d6f71c55134
templexxx <lucas1x1x@gmail.com>
2021-12-23 11:51:01 +0800

    randring/u64: faster catching up when read behind too much & fixing first element poping

--- randring/u64.go
+++ randring/u64.go
@@ -1,6 +1,7 @@
 package randring
 
 import (
+	"math"
 	"sync/atomic"
 
 	"github.com/templexxx/cpu"
@@ -11,15 +12,16 @@ const falseSharingRange = cpu.X86FalseSharingRange
 // U64Ring provides a uint64 ring buckets for multi-producer & one-consumer.
 // For buffer deletion.
 type U64Ring struct {
+	n          uint64
 	mask       uint64
 	_          [falseSharingRange]byte
-	writeIndex uint64
+	writeIndex uint64 // Idx has been written.
 	_          [falseSharingRange]byte
 
 	// writeIndex cache for Pop, only get new write index when read catch write.
 	// Help to reduce caching missing.
 	writeIndexCache uint64
-	readIndex       uint64
+	readIndex       uint64 // Next idx will be read.
 
 	buckets []uint64
 }
@@ -34,6 +36,7 @@ func New(n uint64) *U64Ring {
 
 	r := &U64Ring{
 		buckets: make([]uint64, 1<<n),
+		n:       n,
 		mask:    (1 << n) - 1,
 	}
 
@@ -41,28 +44,37 @@ func New(n uint64) *U64Ring {
 	// to allow the first write to use AddUint64
 	// and still have a beginning index of 0
 	r.writeIndex = ^r.writeIndex
+	r.writeIndexCache = ^r.writeIndexCache
 	return r
 }
 
 // Push puts the data in ring in the next bucket no matter what in it.
 func (r *U64Ring) Push(v uint64) {
 	idx := atomic.AddUint64(&r.writeIndex, 1) & r.mask
-	atomic.SwapUint64(&r.buckets[idx], v)
+	atomic.StoreUint64(&r.buckets[idx], v)
 }
 
 // TryPop tries to pop data from the next bucket,
 // return (nil, false) if no data available.
 func (r *U64Ring) TryPop() (uint64, bool) {
 
-	if r.readIndex >= r.writeIndexCache {
-		r.writeIndexCache = atomic.LoadUint64(&r.writeIndex)
-		if r.readIndex >= r.writeIndexCache {
+	if r.readIndex > r.writeIndexCache || r.writeIndexCache == math.MaxUint64 { // Read catch up write cache.
+		newWIdx := atomic.LoadUint64(&r.writeIndex)
+		if newWIdx == math.MaxUint64 { // Nothing have been written.
+			return 0, false
+		}
+		r.writeIndexCache = newWIdx
+		if r.readIndex > r.writeIndexCache {
 			return 0, false
 		}
 	}
 
+	if r.writeIndexCache-r.readIndex >= r.mask { // Write is ahead of read more/equal than one round, drop all for fast catching up.
+		r.readIndex = r.writeIndexCache
+	}
+
 	idx := r.readIndex & r.mask
-	data := atomic.SwapUint64(&r.buckets[idx], 0)
+	data := atomic.LoadUint64(&r.buckets[idx])
 
 	r.readIndex++
 	return data, true
--- randring/u64_test.go
+++ randring/u64_test.go
@@ -6,14 +6,29 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestU64Ring_TryPopOnlyOne(t *testing.T) {
+	r := New(4)
+	v, ok := r.TryPop()
+	assert.False(t, ok)
+	assert.Equal(t, uint64(0), v)
+	r.Push(1)
+	v, ok = r.TryPop()
+	assert.True(t, ok)
+	assert.Equal(t, uint64(1), v)
+	r.Push(2)
+	v, ok = r.TryPop()
+	assert.True(t, ok)
+	assert.Equal(t, uint64(2), v)
+}
+
 func TestU64Ring_TryPop(t *testing.T) {
 
 	r := New(4)
-	for i := 0; i <= 1<<4; i++ {
+	for i := 0; i < 1<<4; i++ {
 		r.Push(uint64(i))
 	}
 
-	assert.Equal(t, uint64(16), r.writeIndex)
+	assert.Equal(t, uint64(16-1), r.writeIndex)
 
 	cnt := 0
 	for {
@@ -23,5 +38,31 @@ func TestU64Ring_TryPop(t *testing.T) {
 		}
 		cnt++
 	}
-	assert.Equal(t, 16, cnt)
+	assert.Equal(t, 1, cnt)
+}
+
+func TestU64Ring_TryPopSlowPop(t *testing.T) {
+
+	r := New(4)
+
+	for i := 0; i < 1<<4; i++ {
+		r.Push(uint64(i))
+	}
+	r.TryPop()
+	r.TryPop()
+	for i := 16; i < 1<<5; i++ {
+		r.Push(uint64(i))
+	}
+
+	assert.Equal(t, uint64(31), r.writeIndex)
+
+	cnt := 0
+	for {
+		_, ok := r.TryPop()
+		if !ok {
+			break
+		}
+		cnt++
+	}
+	assert.Equal(t, 1, cnt)
 }
